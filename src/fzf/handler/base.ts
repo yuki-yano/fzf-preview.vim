2f48537d4b3cda3483061081eb422a6cdbeeecaf
yuki_yano <yuki.ycino@gmail.com>
2020-06-06 10:31:23 +0900

    Add register store connector

--- src/fzf/handler/base.ts
+++ src/fzf/handler/base.ts
@@ -1,7 +1,27 @@
-import { createOpenFileProcessor } from "@/fzf/processor/open-file"
+import { createConnect, dispatch } from "@/store"
+import { createExecuteCommandSelector } from "@/module/execute-command"
+import { persistModule } from "@/module/persist"
+import { createProcessorsFunctionName } from "@/fzf/processor"
+import { pluginCall } from "@/plugin"
 
 export const baseHandler = (lines: Array<string>) => {
-  const key = lines.shift() as string
-  const openFileProcessor = createOpenFileProcessor()
-  openFileProcessor[key](lines)
+  const connect = createConnect(
+    createExecuteCommandSelector,
+    ({ commandName, options }) => {
+      const expectKey = lines.shift()
+
+      if (commandName && expectKey !== undefined) {
+        pluginCall("fzf_preview#remote#handler_to_processor#receive_from_remote_plugin", [
+          createProcessorsFunctionName(commandName, expectKey),
+          expectKey,
+          lines,
+          options.processorsName
+        ])
+      }
+    },
+    { once: true }
+  )
+  connect()
+
+  dispatch(persistModule.actions.restoreStore())
 }
