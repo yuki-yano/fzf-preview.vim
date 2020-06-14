d30d46f32bcb47032d5223d7785801c6502208c9
yuki_yano <yuki.ycino@gmail.com>
2020-06-14 20:57:10 +0900

    Refactor TSFzfPreviewProjectOldfiles resource

--- src/fzf/resource/project-oldfiles.ts
+++ src/fzf/resource/project-oldfiles.ts
@@ -2,24 +2,18 @@ import { createExecuteCommandSelector } from "@/module/execute-command"
 import { createGlobalVariableSelector } from "@/module/vim-variable"
 import { pluginGetVvar } from "@/plugin"
 import { store } from "@/store"
-import { existsFile } from "@/system/file"
 import { convertForFzf } from "@/system/fzf"
-import { filePathToProjectFilePath, isGitDirectory } from "@/system/project"
-import type { ResourceLine, SourceFuncArgs } from "@/type"
+import { filterProjectEnabledFile, isGitDirectory } from "@/system/project"
+import type { ResourceLine, ResourceLines, SourceFuncArgs } from "@/type"
 
 export const projectOldFiles = async (_args: SourceFuncArgs) => {
   if (!isGitDirectory()) {
     throw new Error("The current directory is not a git project")
   }
 
-  const oldFiles = (await pluginGetVvar("oldfiles")) as Array<string>
-  const existsFiles = oldFiles.filter((file) => existsFile(file))
-
-  const files = existsFiles
-    .map((filePath) => filePathToProjectFilePath(filePath))
-    .filter((filePath): filePath is ResourceLine => filePath !== null)
-
+  const files: ResourceLines = filterProjectEnabledFile((await pluginGetVvar("oldfiles")) as Array<ResourceLine>)
   const executeCommandSelector = createExecuteCommandSelector(store)
+
   const { enableDevIcons } = executeCommandSelector().options
   if (enableDevIcons) {
     return convertForFzf(files)
