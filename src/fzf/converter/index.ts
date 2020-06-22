cd3310dcfa6c8256f4736308217a6496ec8a7e10
yuki_yano <yuki.ycino@gmail.com>
2020-06-22 22:23:14 +0900

    Rename buffer line converter

--- src/fzf/converter/index.ts
+++ src/fzf/converter/index.ts
@@ -1,4 +1,4 @@
 export * from "@/fzf/converter/identity-converter"
 export * from "@/fzf/converter/drop-prefix-converter"
 export * from "@/fzf/converter/split-converter"
-export * from "@/fzf/converter/buffer-line-to-file-converter"
+export * from "@/fzf/converter/line-to-file-converter"
--- src/fzf/converter/line-to-file-converter.ts
+++ src/fzf/converter/line-to-file-converter.ts
@@ -1,7 +1,7 @@
 import { executeCommandSelector } from "@/module/selector/execute-command"
 import type { ConvertedLine, SelectedLine } from "@/type"
 
-export const convertBufferLineToFileAndText = (line: SelectedLine): ConvertedLine => {
+export const convertLineToFileAndText = (line: SelectedLine): ConvertedLine => {
   const { currentFilePath } = executeCommandSelector().options
 
   const [lineNumber, ...text] = line.trim().split(" ")
