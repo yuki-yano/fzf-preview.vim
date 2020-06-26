442fceaaa25686b8eb219f03d54e358b0931ebc6
yuki_yano <yuki.ycino@gmail.com>
2020-06-27 01:47:29 +0900

    Fix settings max buffer size for grep

--- src/system/command.ts
+++ src/system/command.ts
@@ -1,10 +1,11 @@
 import { execSync, SpawnSyncReturns } from "child_process"
 
+import { GREP_MAX_BUFFER_SIZE } from "@/const/system"
 import type { CommandResult } from "@/type"
 
 export const execCommand = (command: string): CommandResult => {
   try {
-    const stdout = execSync(command, { encoding: "utf-8" })
+    const stdout = execSync(command, { encoding: "utf-8", maxBuffer: GREP_MAX_BUFFER_SIZE })
 
     return {
       stdout,
