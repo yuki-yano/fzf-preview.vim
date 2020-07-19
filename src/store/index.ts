7691c0aae5f4442848d03dc6cf1b9e42aaca3660
yuki_yano <yuki.ycino@gmail.com>
2020-07-19 14:56:09 +0900

    Refactor store/store.ts to store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,27 @@
-export * from "@/store/store"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+
+import { cacheModule } from "@/module/cache"
+import { executeCommandModule } from "@/module/execute-command"
+import { resumeModule } from "@/module/resume"
+import { vimVariableModule } from "@/module/vim-variable"
+
+const setupStore = () => {
+  const store = configureStore({
+    reducer: {
+      vimVariable: vimVariableModule.reducer,
+      executeCommand: executeCommandModule.reducer,
+      cache: cacheModule.reducer,
+      resume: resumeModule.reducer,
+    },
+    middleware: getDefaultMiddleware(),
+  })
+
+  return store
+}
+
+export const store = setupStore()
+export const { dispatch } = store
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = typeof store.dispatch
