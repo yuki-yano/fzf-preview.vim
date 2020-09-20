c9b714e585e40d990988f8fcfa4bf6f1af8c11cc
yuki_yano <yuki.ycino@gmail.com>
2020-09-20 18:53:08 +0900

    Fix persist key

--- src/const/module.ts
+++ src/const/module.ts
@@ -1,6 +1,7 @@
 export const PERSIST_LOAD_STORE = "persist/loadStore"
 export const PERSIST_LOAD_CACHE = "persist/loadCache"
 export const PERSIST_LOAD_RESUME = "persist/loadResume"
+export const PERSIST_LOAD_SESSION = "persist/loadSession"
 export const PERSIST_SAVE_STORE = "persist/saveStore"
 
 export const VIM_VARIABLE = "vim_variable"
