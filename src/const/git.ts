48be311bf1d89c31a4f5436356cb62dbdb48a875
PierreMacherel <PierreMacherel@users.noreply.github.com>
2020-10-10 18:25:48 +0200

    feat: detect OS for shell redirection

--- src/const/git.ts
+++ src/const/git.ts
@@ -1,3 +1,7 @@
+import { platform } from "os"
+
+const nullStream = platform() === "win32" ? "nul" : "/dev/null"
+
 export const GIT_ACTIONS = [
   "status",
   "branch",
@@ -46,12 +50,12 @@ export const GIT_STASH_ACTIONS = ["show", "diff", "apply", "pop", "drop", "yank"
 export const GIT_REFLOG_ACTIONS = ["show", "diff", "reset", "reset-hard", "reset-soft", "checkout", "yank"] as const
 
 export const GIT_BRANCH_COMMAND =
-  "git for-each-ref refs/heads refs/remotes --color=always --format='%(color:green)[branch]%(color:reset)    %(color:reset)%(HEAD) %(color:magenta)%(refname:short)%(color:reset)    %(color:yellow)%(authordate:short)%(color:reset)    %(color:blue)[%(authorname)]%(color:reset)%09' 2> /dev/null"
+  "git for-each-ref refs/heads refs/remotes --color=always --format='%(color:green)[branch]%(color:reset)    %(color:reset)%(HEAD) %(color:magenta)%(refname:short)%(color:reset)    %(color:yellow)%(authordate:short)%(color:reset)    %(color:blue)[%(authorname)]%(color:reset)%09' 2> " + nullStream
 
 const GIT_BRANCH_PREVIEW_COMMAND_OPTION =
   "--decorate --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso --graph --color=always"
-export const GIT_BRANCH_PREVIEW_COMMAND = `[[ '{2}' != '*' ]] && git log {2} ${GIT_BRANCH_PREVIEW_COMMAND_OPTION} 2> /dev/null || git log {3} ${GIT_BRANCH_PREVIEW_COMMAND_OPTION} 2> /dev/null`
+export const GIT_BRANCH_PREVIEW_COMMAND = `[[ '{2}' != '*' ]] && git log {2} ${GIT_BRANCH_PREVIEW_COMMAND_OPTION} 2> ${nullStream} || git log {3} ${GIT_BRANCH_PREVIEW_COMMAND_OPTION} 2> ${nullStream}`
 
 export const GIT_LOG_PREVIEW_COMMAND = "git show {2} --color=always"
-export const GIT_STASH_PREVIEW_COMMAND = "git show {2} --color=always 2> /dev/null"
+export const GIT_STASH_PREVIEW_COMMAND = "git show {2} --color=always 2> " + nullStream
 export const GIT_REFLOG_PREVIEW_COMMAND = "git show {2} --color=always"
