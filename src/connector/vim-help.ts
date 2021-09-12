8638cf6d27b7b4203b18d8932a16c09f37c7d3a2
yuki_yano <yuki.ycino@gmail.com>
2021-09-12 16:58:40 +0900

    Fix check plugin directory settings in vim help

--- src/connector/vim-help.ts
+++ src/connector/vim-help.ts
@@ -1,13 +1,13 @@
 import { pluginCall } from "@/plugin"
 
 export const execHelpTags = async (args: string): Promise<ReadonlyArray<string>> => {
-  const grepCommand = `rg --line-number --no-heading --color=never --sort=path ${args} ${
-    process.env.VIMRUNTIME
-  }/doc/* ${
-    process.env.FZF_PREVIEW_PLUGIN_HELP_ROOT_DIR !== ""
-      ? `${process.env.FZF_PREVIEW_PLUGIN_HELP_ROOT_DIR}/**/doc/*`
-      : ""
-  }`
+  const runtimeHelpDir = `${process.env.VIMRUNTIME}/doc/*`
+  const pluginHelpDir =
+    process.env.FZF_PREVIEW_PLUGIN_HELP_ROOT_DIR == null
+      ? ""
+      : `${process.env.FZF_PREVIEW_PLUGIN_HELP_ROOT_DIR}/**/doc/*`
+
+  const grepCommand = `rg --line-number --no-heading --color=never --sort=path ${args} ${runtimeHelpDir} ${pluginHelpDir}`
   const lines = (await pluginCall("fzf_preview#remote#resource#grep#get", [`${grepCommand}`])) as ReadonlyArray<string>
 
   return lines
