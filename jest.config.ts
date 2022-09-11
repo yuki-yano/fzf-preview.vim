c4982f610a2d2669df7e54b9fb18522175313e69
yuki_yano <yuki.ycino@gmail.com>
2022-09-11 21:12:22 +0900

    Fix test

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,9 @@ import type { Config } from "@jest/types"
 
 const config: Config.InitialOptions = {
   verbose: true,
-  moduleFileExtensions: ["js", "ts"],
+  preset: "ts-jest/presets/js-with-ts",
   transform: {
-    "^.+\\.ts$": [
-      "ts-jest",
-      {
-        tsconfig: "tsconfig.json",
-      },
-    ],
+    "^.+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
